6edb2175d3b5b008b9b241aa18b274b60f946c60
AAAplease111 <128509774+AAAplease111@users.noreply.github.com>
2024-01-05 02:11:04 +0800

    Add files via upload

--- src/main/java/ltd/newbee/mall/controller/shopper/ShopperController.java
+++ src/main/java/ltd/newbee/mall/controller/shopper/ShopperController.java
@@ -2,8 +2,8 @@ package ltd.newbee.mall.controller.shopper;
 
 import cn.hutool.captcha.ShearCaptcha;
 import ltd.newbee.mall.common.ServiceResultEnum;
-import ltd.newbee.mall.entity.AdminUser;
-import ltd.newbee.mall.service.AdminUserService;
+import ltd.newbee.mall.entity.ShopperUser;
+import ltd.newbee.mall.service.ShopperUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpSession;
 public class ShopperController {
 
     @Resource
-    private AdminUserService adminUserService;
+    private ShopperUserService shopperUserService;
 
     @GetMapping({"/login"})
     public String login() {
@@ -49,10 +49,10 @@ public class ShopperController {
             session.setAttribute("errorMsg", "验证码错误");
             return "shopper/login";
         }
-        AdminUser adminUser = adminUserService.login(userName, password);
-        if (adminUser != null) {
-            session.setAttribute("loginUser", adminUser.getNickName());
-            session.setAttribute("loginUserId", adminUser.getAdminUserId());
+        ShopperUser shopperUser = shopperUserService.login(userName, password);
+        if (shopperUser != null) {
+            session.setAttribute("loginUser", shopperUser.getNickName());
+            session.setAttribute("loginUserId", shopperUser.getShopperUserId());
             //session过期时间设置为7200秒 即两小时
             //session.setMaxInactiveInterval(60 * 60 * 2);
             return "redirect:/shopper/index";
@@ -65,13 +65,13 @@ public class ShopperController {
     @GetMapping("/profile")
     public String profile(HttpServletRequest request) {
         Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
-        AdminUser adminUser = adminUserService.getUserDetailById(loginUserId);
-        if (adminUser == null) {
+        ShopperUser shopperUser = shopperUserService.getUserDetailById(loginUserId);
+        if (shopperUser == null) {
             return "shopper/login";
         }
         request.setAttribute("path", "profile");
-        request.setAttribute("loginUserName", adminUser.getLoginUserName());
-        request.setAttribute("nickName", adminUser.getNickName());
+        request.setAttribute("loginUserName", shopperUser.getLoginUserName());
+        request.setAttribute("nickName", shopperUser.getNickName());
         return "shopper/profile";
     }
 
@@ -83,7 +83,7 @@ public class ShopperController {
             return "参数不能为空";
         }
         Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
-        if (adminUserService.updatePassword(loginUserId, originalPassword, newPassword)) {
+        if (shopperUserService.updatePassword(loginUserId, originalPassword, newPassword)) {
             //修改成功后清空session中的数据，前端控制跳转至登录页
             request.getSession().removeAttribute("loginUserId");
             request.getSession().removeAttribute("loginUser");
@@ -102,7 +102,7 @@ public class ShopperController {
             return "参数不能为空";
         }
         Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
-        if (adminUserService.updateName(loginUserId, loginUserName, nickName)) {
+        if (shopperUserService.updateName(loginUserId, loginUserName, nickName)) {
             return ServiceResultEnum.SUCCESS.getResult();
         } else {
             return "修改失败";
