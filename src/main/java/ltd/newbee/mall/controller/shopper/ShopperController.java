34a1a845257bb8909bf4ed7721dd3c98abd5d11b
wangxiaoyong <21307110081@m.fudan.edu.cn>
2024-01-04 23:43:12 +0800

    newbee

---
+++ src/main/java/ltd/newbee/mall/controller/shopper/ShopperController.java
@@ -0,0 +1,119 @@
+package ltd.newbee.mall.controller.shopper;
+
+import cn.hutool.captcha.ShearCaptcha;
+import ltd.newbee.mall.common.ServiceResultEnum;
+import ltd.newbee.mall.entity.AdminUser;
+import ltd.newbee.mall.service.AdminUserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/shopper")
+public class ShopperController {
+
+    @Resource
+    private AdminUserService adminUserService;
+
+    @GetMapping({"/login"})
+    public String login() {
+        return "shopper/login";
+    }
+
+
+    @GetMapping({"", "/", "/index", "/index.html"})
+    public String index(HttpServletRequest request) {
+        request.setAttribute("path", "index");
+        return "shopper/index";
+    }
+
+    @PostMapping(value = "/login")
+    public String login(@RequestParam("userName") String userName,
+                        @RequestParam("password") String password,
+                        @RequestParam("verifyCode") String verifyCode,
+                        HttpSession session) {
+        if (!StringUtils.hasText(verifyCode)) {
+            session.setAttribute("errorMsg", "验证码不能为空");
+            return "shopper/login";
+        }
+        if (!StringUtils.hasText(userName) || !StringUtils.hasText(password)) {
+            session.setAttribute("errorMsg", "用户名或密码不能为空");
+            return "shopper/login";
+        }
+        ShearCaptcha shearCaptcha = (ShearCaptcha) session.getAttribute("verifyCode");
+        if (shearCaptcha == null || !shearCaptcha.verify(verifyCode)) {
+            session.setAttribute("errorMsg", "验证码错误");
+            return "shopper/login";
+        }
+        AdminUser adminUser = adminUserService.login(userName, password);
+        if (adminUser != null) {
+            session.setAttribute("loginUser", adminUser.getNickName());
+            session.setAttribute("loginUserId", adminUser.getAdminUserId());
+            //session过期时间设置为7200秒 即两小时
+            //session.setMaxInactiveInterval(60 * 60 * 2);
+            return "redirect:/shopper/index";
+        } else {
+            session.setAttribute("errorMsg", "登录失败");
+            return "shopper/login";
+        }
+    }
+
+    @GetMapping("/profile")
+    public String profile(HttpServletRequest request) {
+        Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
+        AdminUser adminUser = adminUserService.getUserDetailById(loginUserId);
+        if (adminUser == null) {
+            return "shopper/login";
+        }
+        request.setAttribute("path", "profile");
+        request.setAttribute("loginUserName", adminUser.getLoginUserName());
+        request.setAttribute("nickName", adminUser.getNickName());
+        return "shopper/profile";
+    }
+
+    @PostMapping("/profile/password")
+    @ResponseBody
+    public String passwordUpdate(HttpServletRequest request, @RequestParam("originalPassword") String originalPassword,
+                                 @RequestParam("newPassword") String newPassword) {
+        if (!StringUtils.hasText(originalPassword) || !StringUtils.hasText(newPassword)) {
+            return "参数不能为空";
+        }
+        Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
+        if (adminUserService.updatePassword(loginUserId, originalPassword, newPassword)) {
+            //修改成功后清空session中的数据，前端控制跳转至登录页
+            request.getSession().removeAttribute("loginUserId");
+            request.getSession().removeAttribute("loginUser");
+            request.getSession().removeAttribute("errorMsg");
+            return ServiceResultEnum.SUCCESS.getResult();
+        } else {
+            return "修改失败";
+        }
+    }
+
+    @PostMapping("/profile/name")
+    @ResponseBody
+    public String nameUpdate(HttpServletRequest request, @RequestParam("loginUserName") String loginUserName,
+                             @RequestParam("nickName") String nickName) {
+        if (!StringUtils.hasText(loginUserName) || !StringUtils.hasText(nickName)) {
+            return "参数不能为空";
+        }
+        Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
+        if (adminUserService.updateName(loginUserId, loginUserName, nickName)) {
+            return ServiceResultEnum.SUCCESS.getResult();
+        } else {
+            return "修改失败";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("loginUserId");
+        request.getSession().removeAttribute("loginUser");
+        request.getSession().removeAttribute("errorMsg");
+        return "shopper/login";
+    }
+}
